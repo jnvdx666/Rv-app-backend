a5039376d265c960ad7102e55315357a1cc8a71c
jnvdx666 <jon.agramonte@opendeusto.es>
2022-02-24 22:53:20 +0100

    backend debug

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b5phbuybgrme-uqp6i$bc-n#@*2hg&e3!!q-g%3#!5tpf8&azp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
